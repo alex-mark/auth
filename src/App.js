833dbf94eca6b74fc07b6c80aa51cb42bd6eff4c
alex-mark <alexndr86@gmail.com>
2017-09-19 18:20:07 +0300

    login added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+
+  componentDidMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyC9Z5Iw6XKuc2Z1UvSDzoX0CN2pB_wER8I',
+      authDomain: 'auth-fc113.firebaseapp.com',
+      databaseURL: 'https://auth-fc113.firebaseio.com',
+      projectId: 'auth-fc113',
+      storageBucket: 'auth-fc113.appspot.com',
+      messagingSenderId: '181643612245'
+    });
+  }
+
   render() {
     return (
       <View>
